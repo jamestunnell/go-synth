25e972e98825c4d2c2d664983b6f2a44da38c1c8
James Tunnell <jamestunnell@gmail.com>
2020-08-13 13:07:27 -0700

    change line endings to LF

--- api/readrequestdata.go
+++ api/readrequestdata.go
@@ -7,16 +7,16 @@ import (
 )
 
 func ReadRequestData(r *http.Request) ([]byte, error) {
-	defer func(){
+	defer func() {
 		if err := r.Body.Close(); err != nil {
 			log.Printf("failed to close request body: %v", err)
 		}
 	}()
-	
+
 	data, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		return []byte{}, err
 	}
-	
+
 	return data, nil
 }
