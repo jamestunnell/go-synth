67bdf5d30d9c62e1dbe0b26599be02943e39856c
James Tunnell <jamestunnell@gmail.com>
2020-10-09 12:15:59 -0700

    Add proc to negate

---
+++ proc/neg/negate.go
@@ -0,0 +1,41 @@
+package neg
+
+import (
+	"github.com/jamestunnell/go-synth/node"
+)
+
+type negate struct {
+	in node.Node
+
+	outBuf, inBuf *node.Buffer
+}
+
+func Negate(in node.Node) node.Node {
+	return &negate{in: in}
+}
+
+func (neg *negate) Buffer() *node.Buffer {
+	return neg.outBuf
+}
+
+func (neg *negate) Controls() map[string]node.Node {
+	return map[string]node.Node{}
+}
+
+func (neg *negate) Inputs() map[string]node.Node {
+	return map[string]node.Node{"in": neg.in}
+}
+
+func (neg *negate) Initialize(srate float64, depth int) {
+	neg.outBuf = node.NewBuffer(depth)
+	neg.inBuf = neg.in.Buffer()
+}
+
+func (neg *negate) Configure() {
+}
+
+func (neg *negate) Run() {
+	for i := 0; i < neg.outBuf.Length; i++ {
+		neg.outBuf.Values[i] = -neg.inBuf.Values[i]
+	}
+}
---
+++ proc/neg/negate_test.go
@@ -0,0 +1,22 @@
+package neg_test
+
+import (
+	"testing"
+
+	"github.com/jamestunnell/go-synth/gen/array"
+	"github.com/jamestunnell/go-synth/node"
+	"github.com/jamestunnell/go-synth/proc/neg"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInvertHappyPath(t *testing.T) {
+	in := array.OneShot([]float64{1.0, 0.5, -0.5})
+	negate := neg.Negate(in)
+
+	node.Initialize(negate, 100.0, 3)
+	node.Run(negate)
+
+	assert.Equal(t, -1.0, negate.Buffer().Values[0])
+	assert.Equal(t, -0.5, negate.Buffer().Values[1])
+	assert.Equal(t, 0.5, negate.Buffer().Values[2])
+}
