f5d7c49c9077043580070a15689041af5b118060
James Tunnell <jamestunnell@gmail.com>
2022-12-04 21:18:46 -0800

    Update RenderWAV

--- util/renderwav.go
+++ util/renderwav.go
@@ -1,13 +1,13 @@
 package util
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/go-audio/audio"
 	"github.com/go-audio/transforms"
 	"github.com/go-audio/wav"
-
-	"github.com/jamestunnell/go-synth/node"
+	"github.com/jamestunnell/go-synth"
 )
 
 // RenderParams are used in rendering a WAV file
@@ -15,6 +15,26 @@ type RenderParams struct {
 	SampleRate int
 	DurSec     float64
 	BitDepth   int
+}
+
+type ErrWrongOutputCount struct {
+	Expected, Actual int
+}
+
+type ErrWrongOutputType struct {
+	Expected, Actual string
+}
+
+func (err *ErrWrongOutputCount) Error() string {
+	const strFmt = "block has wrong output count: expected %d, actual %d"
+
+	return fmt.Sprintf(strFmt, err.Expected, err.Actual)
+}
+
+func (err *ErrWrongOutputType) Error() string {
+	const strFmt = "block has wrong output type: expected %s, actual %s"
+
+	return fmt.Sprintf(strFmt, err.Expected, err.Actual)
 }
 
 // NumSamples determines the total number of samples based on
@@ -28,18 +48,40 @@ const FormatPCM = 1
 
 // RenderWAV renders audio to a WAV file.
 // Returns a non-nil error in case of failure.
-func RenderWAV(out *node.Node, wavFile *os.File, params *RenderParams) error {
+func RenderWAV(src synth.Block, wavFile *os.File, params *RenderParams) error {
 	numSamples := params.NumSamples()
 
-	buffer := &audio.FloatBuffer{
+	dstBuf := &audio.FloatBuffer{
 		Format: &audio.Format{NumChannels: 1, SampleRate: params.SampleRate},
 		Data:   make([]float64, numSamples),
 	}
 
-	chunkSize := out.Output().Length
+	ifc := synth.GetInterface(src)
+	if len(ifc.Outputs) != 1 {
+		return &ErrWrongOutputCount{
+			Expected: 1,
+			Actual:   len(ifc.Outputs),
+		}
+	}
+
+	var output synth.Output
+
+	for _, o := range ifc.Outputs {
+		if o.Type() != "float64" {
+			return &ErrWrongOutputType{
+				Expected: "float64",
+				Actual:   o.Type(),
+			}
+		}
+
+		output = o
+	}
+
+	srcBuf := output.Buffer().([]float64)
+	chunkSize := len(srcBuf)
 
 	for i := 0; i < numSamples; i += chunkSize {
-		out.Run()
+		src.Run()
 
 		jLim := chunkSize
 		if i+chunkSize > numSamples {
@@ -47,22 +89,22 @@ func RenderWAV(out *node.Node, wavFile *os.File, params *RenderParams) error {
 		}
 
 		for j := 0; j < jLim; j++ {
-			buffer.Data[i+j] = out.Output().Values[j]
+			dstBuf.Data[i+j] = srcBuf[j]
 		}
 	}
 
 	// Clip any samples that are not within the range (-1,1)
 	for i := 0; i < numSamples; i++ {
-		if buffer.Data[i] >= 1.0 {
-			buffer.Data[i] = 1.0 - 1e-5
-		} else if buffer.Data[i] <= -1.0 {
-			buffer.Data[i] = -1.0 + 1e-5
+		if dstBuf.Data[i] >= 1.0 {
+			dstBuf.Data[i] = 1.0 - 1e-5
+		} else if dstBuf.Data[i] <= -1.0 {
+			dstBuf.Data[i] = -1.0 + 1e-5
 		}
 	}
 
-	transforms.PCMScale(buffer, params.BitDepth)
+	transforms.PCMScale(dstBuf, params.BitDepth)
 
-	intBuffer := buffer.AsIntBuffer()
+	intBuffer := dstBuf.AsIntBuffer()
 	intBuffer.SourceBitDepth = params.BitDepth
 
 	return writeWAV(wavFile, intBuffer, params.BitDepth)
